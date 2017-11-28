d0874e588410a7504f87d9f18d95b2dd4355f9fa
lawrencedc <lawrence.delcarmen@gmail.com>
2017-11-28 16:56:01 +0800

    Initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
+
+import { routing } from './app.routing';
+import { AppComponent } from './app.component';
+import { Session } from '../session/session';
+
+import { LoginComponent } from './login/login.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { CookieService } from 'ngx-cookie-service';
+
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    routing,
+    ToastModule.forRoot()
+  ],
+  providers: [
+    CookieService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,128 @@
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { Http } from '@angular/http';
+import { URLSearchParams } from '@angular/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { CookieService } from 'ngx-cookie-service';
+
+import {
+    FormGroup,
+    FormControl,
+    Validators
+} from '@angular/forms';
+
+@Component({
+   templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit {
+    constructor(
+        private _httpService: Http,
+        private route: ActivatedRoute,
+        private router: Router,
+        private toastr: ToastsManager,
+        private cookieService: CookieService,
+        vcr: ViewContainerRef
+    ) { this.toastr.setRootViewContainerRef(vcr);
+    }
+
+    private returnUrl: string;
+
+    // login form
+    private loginform: FormGroup;
+    private usernameLogin: FormControl;
+    private passwordLogin: FormControl;
+
+    private apiUrl = 'http://localhost:4066/api/token';
+
+    ngOnInit() {
+        if (this.cookieService.get('access_token')) {
+            console.log('Cookie Not Expired');
+            this.router.navigate(['/']);
+        } else {
+            console.log('Expired');
+            this.removeCookie();
+        }
+
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+        this.createFormControls();
+        this.createForm();
+    }
+
+    createFormControls() {
+        this.usernameLogin = new FormControl('', Validators.required);
+        this.passwordLogin = new FormControl('', Validators.required);
+    }
+
+    createForm() {
+        this.loginform = new FormGroup({
+            username: this.usernameLogin,
+            password: this.passwordLogin
+        });
+    }
+
+    removeCookie() {
+        this.cookieService.deleteAll();
+        localStorage.clear();
+    }
+
+    login() {
+        if (this.loginform.valid) {
+            const body = new URLSearchParams();
+            body.set('username', this.loginform.value['username']); // 'yourchapter/testcgn19284@mychapterroom.com');
+            body.set('password', this.loginform.value['password']);
+            body.set('grant_type', 'password');
+            interface RootObject {
+                _body: string;
+                status: number;
+                ok: boolean;
+                statusText: string;
+                headers: Headers;
+                type: number;
+                url: string;
+            }
+
+            interface AccessToken {
+                access_token: string;
+                token_type: string;
+                expires_in: number;
+                User: string;
+                MemberId: string;
+                MemberType: string;
+                ChapterId: string;
+                BondId: string;
+                FirstName: string;
+                LastName: string;
+                IsAdmin: string;
+            }
+
+            this._httpService.post(this.apiUrl, body)
+                .subscribe(
+                    data => {
+                        this.loginform.reset();
+                        const response = JSON.stringify(data);
+                        const jsonObj: any = JSON.parse(response);
+                        const obj: RootObject = <RootObject>jsonObj;
+                        const accessToken: AccessToken = <AccessToken>JSON.parse(obj._body);
+
+                        localStorage.setItem('MemberId', accessToken.MemberId);
+                        localStorage.setItem('MemberType', accessToken.MemberType);
+                        localStorage.setItem('ChapterId', accessToken.ChapterId);
+                        localStorage.setItem('FirstName', accessToken.FirstName);
+                        localStorage.setItem('LastName', accessToken.LastName);
+                        localStorage.setItem('IsAdmin', accessToken.IsAdmin);
+
+                        this.cookieService.set('access_token', accessToken.access_token, accessToken.expires_in);
+                        this.cookieService.set('token_type', accessToken.token_type, accessToken.expires_in);
+                        this.router.navigate([this.returnUrl]);
+                    },
+                    error => {
+                        if (error.status === 404) {
+                            return this.toastr.warning('Account Not Found', '404');
+                        }
+
+                        this.toastr.error('Error in logging in.', 'Oops!');
+                    }
+                );
+        }
+    }
+}
